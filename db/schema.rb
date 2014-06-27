190912ff2d9e428b6c4bc7fdc089dac152c9ec17
Yilin Chen <wormful@gmail.com>
2014-06-27 21:57:13 +0800

    Move assets to qiniu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621080128) do
+ActiveRecord::Schema.define(version: 20140626133614) do
+
+  create_table "activities", force: true do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
